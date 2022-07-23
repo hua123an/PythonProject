21d58ab6046629409f803911371509e744f78a70
hua123an <1994843581@qq.com>
2022-07-24 07:10:24 +0800

    Changes

---
+++ Exercise/liangshuqiuhe.py
@@ -0,0 +1,11 @@
+num1=float(2.0) #输入第一个数
+
+
+
+num2=float(3.0)      #输入第二个数
+
+
+sum = float(num1)+float(num2)
+
+
+print(format(num1,num2,sum))
---
+++ Exercise/panduanyuansuzucheng.py
@@ -0,0 +1,3 @@
+str = "hello world 666"  #输入要判断的字符串
+
+print(str.isalnum())  #
