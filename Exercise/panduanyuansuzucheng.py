8b25ed302d75917f524d83e6f38fbbc854c0c4bb
hua123an <1994843581@qq.com>
2022-07-24 07:19:40 +0800

    Changes

--- Exercise/panduanyuansuzucheng.py
+++ Exercise/panduanyuansuzucheng.py
@@ -1,3 +1,13 @@
 str = "hello world 666"  #输入要判断的字符串
 
-print(str.isalnum())  #
+print(str.isalnum())  #判断所有字符都是字母或数字
+
+print(str.isalpha())  #判断所有字母都是字母
+
+print(str.isdigit())  #判断所有字符串都是数字
+
+print(str.islower())  #判断所有字符串都是小写
+
+print(str.isupper())  #判断所有字符串都是大写
+
+print(str.istitle())  #判断字符串像文章题目一样
