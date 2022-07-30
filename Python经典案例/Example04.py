a0ef10b3102a3909534d79b54a90a2b7a00ee856
hua123an <97239258+hua123an@users.noreply.github.com>
2022-07-30 22:35:17 +0800

    Changes

---
+++ Python经典案例/Example04.py
@@ -0,0 +1,10 @@
+#统计字符串中给定字母的个数
+def test():
+    message = "hello world ,welcome to my world"
+    #计数
+    num = 0
+    for i in range:
+        if 'w' in i:
+            num +=1
+            return num
+        print(test())
